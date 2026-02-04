da677fc4a0627c558f65fe5af73db0c95451270f
Brahvim <69293652+Brahvim@users.noreply.github.com.>
2026-02-04 23:47:21 +0530

    Finally, object-oriented C (No Virtuals Edition!).
    I try not to make these things, okay?! But I think I gotta' take it further. Why not?!

--- Dfa.c
+++ Dfa.c
@@ -12,38 +12,48 @@ dfaAlphabetEnumBody(
 
 int main(int const p_argCount, char const *const p_argValues[]) {
 	struct StatesAb *q0 = calloc(1, sizeof(struct StatesAb));
-	struct StatesAb *q1 = statePtr(Ab, "q1", NULL);
-	// *q0 = stateVal(Ab, "q0", q1);
-	*q0 = ((struct StatesAb) { "q0", (struct StatesAb const *const) { q1 } });
-}
+	struct StatesAb *q1 = calloc(1, sizeof(struct StatesAb));
+	q0->transitions = calloc(2, sizeof(struct StatesAb));
+	*(q0->transitions + 0) = q0;
+	*(q0->transitions + 1) = q1;
+	q1->transitions = NULL;
+	q0->name = "q0";
+	q1->name = "q1";
 
-/*
-int main() {
-	char const *const input = "bba";
-	struct State state = stateVal(q0);
-	size_t const len = strlen(input);
+	enum AlphabetsAb const input[] = {
+
+		ALPHABET_AB_A,
+		ALPHABET_AB_B,
+
+	};
+	size_t const len = sizearr(input);
+	struct StatesAb *state = q0;
 
 	for (size_t i = 0; i < len; ++i) {
 
-		if (!state.transition) { // ...Or `exit()` in `q1()`?
+		if (!state->transitions) { // ...Or `exit()` in `q1()`?
 
 			break;
 
 		}
 
-		struct State const *const prev = &state;
-		char const character = *(input + i);
-		state = state.transition(character);
+		struct StatesAb const *const prev = state;
+		enum AlphabetsAb symbol = *(input + i);
+		state = *(state->transitions + symbol);
+
 		printf(
-			state.transition == state.transition
-				? "Input `%c` does not transform machine state.\n"
-				: "Input `%c` transforms machine state from `%s` -> `%s`.\n",
-			state.name,
-			prev->name
+			state == prev
+				? "Input `%d` does not transform machine state.\n"
+				: "Input `%d` transforms machine state from `%s` -> `%s`.\n",
+			symbol,
+			prev->name,
+			state->name
 		);
 
 	}
 
+	free(q0->transitions);
+	free(q0);
+	free(q1);
 	exit(EXIT_SUCCESS);
 }
-*/
--- Dfa.h
+++ Dfa.h
@@ -1,10 +1,12 @@
 #pragma once
 
+#define sizearr(p_array) sizeof(p_array) / sizeof(p_array[0])
+
 #define statePtr(p_apbet, p_name, ...)	\
 (&stateVal(p_apbet, p_name, __VA_ARGS__))
 
 #define stateVal(p_apbet, p_name, ...)	\
-((struct States ## p_apbet) { p_name, (struct States ## p_apbet const *const) { __VA_ARGS__ } })
+((struct States ## p_apbet) { p_name, (struct States ## p_apbet*) { __VA_ARGS__ } })
 
 #define stateVar(p_apbet, p_var, p_name, ...)	\
 struct States ## p_apbet p_var = stateVal(p_apbet, p_name, __VA_ARGS__)
@@ -19,17 +21,17 @@ dfaAlphabetEnumEntry(p_apbet, TOTAL)
 ALPHABET_ ## p_apbet ## _ ## p_entry
 
 #define dfaAlphabetEnumBody(p_enum, p_apbet, ...)	\
-enum Alphabets ## p_struct	{ 						\
+enum Alphabets ## p_enum { 							\
 													\
 	__VA_ARGS__,									\
 	dfaAlphabetTotal(p_apbet)						\
 													\
 }
 
-#define dfaStateStruct(p_struct)						\
-struct States ## p_struct {								\
-														\
-	char const *name;									\
-	struct States ## p_struct const *const transitions;	\
-														\
+#define dfaStateStruct(p_struct)				\
+struct States ## p_struct {						\
+												\
+	char const *name;							\
+	struct States ## p_struct **transitions;	\
+												\
 }
